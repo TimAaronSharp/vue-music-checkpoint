b0eb8cfee8645b1d695ac0c6bf6c76eaea32c4c9
Tim Sharp <timothyaaron.sharp@gmail.com>
2018-01-20 18:13:27 -0700

    asd

--- public/src/store/mytunes-store.js
+++ public/src/store/mytunes-store.js
@@ -38,9 +38,9 @@ var store = new vuex.Store({
   },
   actions: {
     getMusicByArtist({ commit, dispatch }, artist) {
-      var url = '//bcw-getter.herokuapp.com/?url=';
+      // var url = '//bcw-getter.herokuapp.com/?url=';
       var url2 = 'https://itunes.apple.com/search?term=' + artist;
-      var apiUrl = url + encodeURIComponent(url2);
+      var apiUrl = encodeURIComponent(url2);
       $.get(url2).then(data => {
         data = JSON.parse(data)
         commit('setResults', data.results)
